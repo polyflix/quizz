8c8822f9b3aa293adf2e8a23c0311d62fefcdf7d
leofvo <leofvo@pm.me>
2022-05-03 13:26:34 +0200

    feat(typeorm): added typeorm

---
+++ src/main/config/database.config.ts
@@ -0,0 +1,39 @@
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ISLOCAL } from "./loader.config";
+import { resolve } from "path";
+import { writeFileSync } from "fs";
+
+export const configureTypeORM = (
+  configService: ConfigService
+): TypeOrmModuleOptions => {
+  const config: TypeOrmModuleOptions = {
+    type: "postgres",
+    host: configService.get("database.psql.host"),
+    port: configService.get("database.psql.port"),
+    username: configService.get("database.psql.username"),
+    password: configService.get("database.psql.password"),
+    database: configService.get("database.psql.database"),
+    synchronize: ISLOCAL,
+    migrationsTableName: "migrations",
+    entities: ["dist/**/*.entity.js"],
+    logging: configService.get("database.psql.debug") === "true",
+    cli: {
+      migrationsDir: "src/resources/migrations"
+    },
+    autoLoadEntities: true
+  };
+
+  // Write TypeORM json config
+  const path = resolve("ormconfig.json");
+  writeFileSync(
+    path,
+    JSON.stringify({
+      ...config,
+      entities: ["src/**/*.entity.ts"],
+      migrations: ["src/resources/migrations/*.ts"]
+    })
+  );
+
+  return config;
+};
