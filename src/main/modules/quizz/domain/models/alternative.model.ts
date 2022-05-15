f495a49380d349a30eeafd8b3fe6b0e4e3eaf885
leofvo <leofvo@pm.me>
2022-05-15 22:24:27 +0200

    refactor(quizz): fixed integration issues with frontend

--- src/main/modules/quizz/domain/models/alternative.model.ts
+++ src/main/modules/quizz/domain/models/alternative.model.ts
@@ -1,6 +1,8 @@
+import { Question } from "./question.model";
 import { Default } from "./default.model";
 
 export class Alternative extends Default {
   label: string;
   isCorrect: boolean;
+  question?: Question;
 }
