49cc8e04073b8268874b852740f6cc5a8b0f668f
leofvo <leofvo@pm.me>
2022-05-23 09:49:06 +0200

    fix: minor fix + user refactor
    error handling
    user as flat entity in quizzEntity
    repository join

--- src/main/modules/quizz/domain/models/alternative.model.ts
+++ src/main/modules/quizz/domain/models/alternative.model.ts
@@ -1,8 +1,6 @@
-import { Question } from "./question.model";
 import { Default } from "./default.model";
 
 export class Alternative extends Default {
   label: string;
   isCorrect: boolean;
-  question?: Question;
 }
