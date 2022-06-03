700d19c25b48bdb00f0aeb0407d226b8c6efb55a
leofvo <leofvo@pm.me>
2022-06-03 13:01:29 +0200

    fix(migrations): add draft to entity

---
+++ src/resources/migrations/1654254057409-AddDraftForQuizz.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddDraftForQuizz1654254057409 implements MigrationInterface {
+    name = 'AddDraftForQuizz1654254057409'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "quizz" ADD "draft" boolean NOT NULL DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "quizz" DROP COLUMN "draft"`);
+    }
+
+}
